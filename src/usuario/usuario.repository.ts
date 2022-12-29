6739fe8414adaface26944145fa0a772697c93ff
RaphaellaMorais <raphaellakm@gmail.com>
2022-12-29 00:45:50 -0300

    messagem

--- src/usuario/usuario.repository.ts
+++ src/usuario/usuario.repository.ts
@@ -27,7 +27,7 @@ export class UsuarioRepository {
     );
 
     if (!possivelUsuario) {
-      throw new Error('Usuário não existe.');
+      throw new Error('Usuário não existe!');
     }
 
     return possivelUsuario;
